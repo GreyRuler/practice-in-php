d57ff24b716248927db7ed38fcb4b0aa5ee1e477
GreyRuler <daniil_vasilenko757@mail.ru>
2022-02-09 14:14:38 +0300

    Add files

---
+++ Practice by the book David Sklar/Chapter 2/1.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Найдите ошибки в приведенном ниже исходном тексте программы на РНР.
+ * <? php
+ * print 'How are you?';
+ * print 'I'm fine.';
+ * ??>
+ */
+print 'How are you?';
+print 'I\'m fine.';
+?>
